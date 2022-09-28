4313330281b79e82222e39730d96cf4478ce21ad
Matthew <matthewtoro17@gmail.com>
2022-09-29 00:00:04 +0800

    Create append function of list

---
+++ linkedList.js
@@ -0,0 +1,31 @@
+const nodeFactory = (value, nextNode) => { 
+  return { value, nextNode }; 
+};
+
+const linkedListFactory = () => {
+  let head = null;
+
+  const append = (value) => {
+    if (head === null) {
+      head = nodeFactory(value, null);
+    } else {
+      let currentNode = head;
+      
+      while (currentNode.nextNode !== null) {
+        currentNode = currentNode.nextNode;
+      }
+
+      currentNode.nextNode = nodeFactory(value, null);
+    }
+  };
+
+  const getHead = () => console.log(head);
+
+  return { append, getHead };
+};
+
+const list = linkedListFactory();
+list.append('test');
+list.append('test2');
+
+list.getHead();
